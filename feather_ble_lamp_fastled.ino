85d687e94a556f87773cc23cf0f42dbc642d6b70
Jason Coon <jasoncoon@users.noreply.github.com>
2016-02-26 17:02:47 -0600

    Added files via upload

---
+++ feather_ble_lamp_fastled.ino
@@ -0,0 +1,439 @@
+#include <FastLED.h>
+#include "GradientPalettes.h"
+
+FASTLED_USING_NAMESPACE
+
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_SPI.h>
+#include <Adafruit_BluefruitLE_UART.h>
+
+#include "BluefruitConfig.h"
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+#define DATA_PIN    6
+//#define CLK_PIN   4
+#define LED_TYPE    NEOPIXEL
+//#define COLOR_ORDER RGB
+#define NUM_LEDS    24
+
+#define VBATPIN A9
+
+CRGB leds[NUM_LEDS];
+
+#define FRAMES_PER_SECOND  120
+
+// ten seconds per color palette makes a good demo
+// 20-120 is better for deployment
+#define SECONDS_PER_PALETTE 10
+
+uint8_t brightness = 96;
+
+void setup() {
+  Serial.begin(115200);
+
+  //  for(uint8_t i = 0; i < 10; i++) {
+  //    Serial.println("Startup delay...");
+  //    delay(1000);
+  //  }
+
+  pinMode(13, OUTPUT);
+
+  FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(brightness);
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+
+  setupBLE();
+}
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+// function prototypes over in packetparser.cpp
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+float parsefloat(uint8_t *buffer);
+void printHex(const uint8_t * data, const uint32_t numBytes);
+
+// the packet buffer
+extern uint8_t packetbuffer[];
+
+void setupBLE() {
+  Serial.println(F("Adafruit Bluefruit Neopixel Color Picker Example"));
+  Serial.println(F("------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  //  /* Wait for connection */
+  //  while (! ble.isConnected()) {
+  //      delay(500);
+  //  }
+
+  Serial.println(F("***********************"));
+
+  // Set Bluefruit to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("***********************"));
+}
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = {
+  pride,
+  colorwaves,
+  rainbow,
+  rainbowWithGlitter,
+  confetti,
+  sinelon,
+  juggle,
+  bpm,
+  showSolidColor,
+};
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+const uint8_t patternCount = ARRAY_SIZE( gPatterns);
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+// Forward declarations of an array of cpt-city gradient palettes, and
+// a count of how many there are.  The actual color palette definitions
+// are at the bottom of this file.
+extern const TProgmemRGBGradientPalettePtr gGradientPalettes[];
+extern const uint8_t gGradientPaletteCount;
+
+// Current palette number from the 'playlist' of color palettes
+uint8_t gCurrentPaletteNumber = 0;
+
+CRGBPalette16 gCurrentPalette( CRGB::Black);
+CRGBPalette16 gTargetPalette( gGradientPalettes[0] );
+
+CRGB solidColor = CRGB::Blue;
+
+void loop() {
+  handleInput();
+  // blinkStatusLED();
+
+  EVERY_N_SECONDS(10) {
+    measurePower();
+  }
+
+  EVERY_N_SECONDS( SECONDS_PER_PALETTE ) {
+    gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
+    gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
+  }
+
+  EVERY_N_MILLISECONDS(40) {
+    nblendPaletteTowardPalette( gCurrentPalette, gTargetPalette, 16);
+  }
+
+  // Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) {
+    gHue++;  // slowly cycle the "base color" through the rainbow
+  }
+}
+
+void handleInput() {
+  uint8_t len = readPacket(&ble, BLE_READPACKET_TIMEOUT);
+
+  // Color
+  if (packetbuffer[1] == 'C') {
+    solidColor.red = packetbuffer[2];
+    solidColor.green = packetbuffer[3];
+    solidColor.blue = packetbuffer[4];
+    Serial.print ("RGB #");
+    if (solidColor.red < 0x10) Serial.print("0");
+    Serial.print(solidColor.red, HEX);
+    if (solidColor.green < 0x10) Serial.print("0");
+    Serial.print(solidColor.green, HEX);
+    if (solidColor.blue < 0x10) Serial.print("0");
+    Serial.println(solidColor.blue, HEX);
+    gCurrentPatternNumber = patternCount - 1;
+  }
+
+  // Buttons
+  if (packetbuffer[1] == 'B') {
+    uint8_t buttonNumber = packetbuffer[2] - '0';
+    boolean pressed = packetbuffer[3] - '0';
+    Serial.print ("Button "); Serial.print(buttonNumber);
+    if (pressed) {
+      Serial.println(" pressed");
+      
+      switch (buttonNumber) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+          gCurrentPatternNumber = (buttonNumber - 1) % patternCount;
+          break;
+
+        case 5: // up
+          movePattern(1);
+          break;
+
+        case 6: // down
+          movePattern(-1);
+          break;
+
+        case 7: // left
+          brightness -= 32;
+          FastLED.setBrightness(brightness);
+          break;
+
+        case 8: // right
+          brightness += 32;
+          FastLED.setBrightness(brightness);
+          break;
+      }
+    }
+    else {
+      Serial.println(" released");
+    }
+  }
+}
+
+void blinkStatusLED() {
+  static bool ledHigh = true;
+
+  EVERY_N_SECONDS(1) {
+    ledHigh = !ledHigh;
+  }
+
+  if (ledHigh) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+}
+
+void measurePower() {
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  Serial.print("VBat: " ); Serial.println(measuredvbat);
+}
+
+void movePattern(int direction)
+{
+  if(gCurrentPatternNumber == 0 && direction < 0)
+    gCurrentPatternNumber = patternCount - 1;
+  else if (gCurrentPatternNumber == patternCount - 1 && direction > 0)
+    gCurrentPatternNumber = 0;
+  else
+    gCurrentPatternNumber += direction;
+
+  if(gCurrentPatternNumber >= patternCount)
+    gCurrentPatternNumber = 0;
+}
+
+void showSolidColor()
+{
+  fill_solid(leds, NUM_LEDS, solidColor);
+}
+
+void rainbow()
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter()
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti()
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16(13, 0, NUM_LEDS);
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for ( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for ( int i = 0; i < 8; i++) {
+    leds[beatsin16(i + 7, 0, NUM_LEDS)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+// This function draws rainbows with an ever-changing,
+// widely-varying set of parameters.
+void pride()
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5, 9);
+  uint16_t brightnesstheta16 = sPseudotime;
+
+  for ( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
+
+    nblend( leds[pixelnumber], newcolor, 64);
+  }
+}
+
+// This function draws color waves with an ever-changing,
+// widely-varying set of parameters, using a color palette.
+void colorwaves()
+{
+  // colorwaves( leds, NUM_LEDS, gCurrentPalette);
+
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 300, 1500);
+
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5, 9);
+  uint16_t brightnesstheta16 = sPseudotime;
+
+  for ( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+    uint16_t h16_128 = hue16 >> 7;
+    if ( h16_128 & 0x100) {
+      hue8 = 255 - (h16_128 >> 1);
+    } else {
+      hue8 = h16_128 >> 1;
+    }
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+
+    uint8_t index = hue8;
+    //index = triwave8( index);
+    index = scale8( index, 240);
+
+    CRGB newcolor = ColorFromPalette( gCurrentPalette, index, bri8);
+
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
+
+    nblend( leds[pixelnumber], newcolor, 128);
+  }
+}
+
+// Alternate rendering function just scrolls the current palette
+// across the defined LED strip.
+void palettetest( CRGB* ledarray, uint16_t numleds, const CRGBPalette16& gCurrentPalette)
+{
+  static uint8_t startindex = 0;
+  startindex--;
+  fill_palette( ledarray, numleds, startindex, (256 / NUM_LEDS) + 1, gCurrentPalette, 255, LINEARBLEND);
+}
